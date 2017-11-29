5e94ec3893afcad7c22e99c9d8d79b98610cdacc
Wes Bos <wesbos@gmail.com>
2017-11-29 11:46:41 -0500

    local images because conf wifi suxxx

--- image-downloader.js
+++ image-downloader.js
@@ -1,43 +1,31 @@
 let fs = require('fs'),
   request = require('request');
 
-let download = function(callback) {
+const download = function(callback) {
   const uri = images.pop(); // take the last one off
   request.head(uri, (err, res, body) => {
     const extension = res.headers['content-type'].split('/').pop();
     const fileName = `${uri.split('/')[3]}.${extension}`;
-    console.log("downloading...", fileName);
-    request(uri).pipe(fs.createWriteStream('images/screenshots/' + fileName)).on('close', callback);
+    console.log('downloading...', fileName);
+    request(uri)
+      .pipe(fs.createWriteStream('images/screenshots/' + fileName))
+      .on('close', callback);
   });
 };
 
 var images = [
-  'http://wes.io/mw8s/content',
-  'http://wes.io/mvNc/content',
-  'http://wes.io/mvvk/content',
-  'http://wes.io/mvlO/content',
-  'http://wes.io/mw1Y/content',
-  'http://wes.io/mvjp/content',
-  'http://wes.io/mul6/content',
-  'http://wes.io/mul6/content',
-  'http://wes.io/mv6R/content',
-  'http://wes.io/mufS/content',
-  'http://wes.io/mw88/content',
-  'http://wes.io/mvxm/content',
-  'http://wes.io/mvLf/content',
-  'http://wes.io/mvHv/content',
-  'http://wes.io/mvPr/content',
-  'http://wes.io/muk4/content',
-  'http://wes.io/mupq/content',
-  'http://wes.io/mvSI/content',
-  'http://wes.io/mvbJ/content',
-  'http://wes.io/mvMC/content',
-  'http://wes.io/mvMC/content',
-  'http://wes.io/mw55/content',
-  'http://wes.io/mv93/content',
-  'http://wes.io/musk/content',
-  'http://wes.io/mvNd/content',
-  'http://wes.io/mvjO/content',
+  'http://wes.io/nslE/content',
+  'http://wes.io/nsg8/content',
+  'http://wes.io/nsi4/content',
+  'http://wes.io/nsbK/content',
+  'http://wes.io/nsbz/content',
+  'http://wes.io/ntkI/content',
+  'http://wes.io/nuqs/content',
+  'http://wes.io/nunX/contnet',
+  'http://wes.io/nteh/content',
+  'http://wes.io/nuda/content',
+  'http://wes.io/nyqR/content',
+  'http://wes.io/ntqa/content',
 ];
 
 function start() {
